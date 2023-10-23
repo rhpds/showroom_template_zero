b804db36d50003c9d708db8529c23c53e1d218b9
Tony Kay <tony.g.kay@gmail.com>
2023-10-23 12:46:06 -0600

    Major restructure for simplified developer UX

--- utilities/build.sh
+++ utilities/build.sh
@@ -5,6 +5,7 @@ echo "Starting build process..."
 echo "Removing old site..."
 rm -rf ./www/*
 echo "Building new site..."
-# podman run --rm --name showroom-builder --platform linux/amd64 -v "./:/antora" docker.io/antora/antora zero-touch-site.yml
-podman run --rm --name showroom-builder --platform linux/amd64 -v "./:/antora" docker.io/antora/antora site.yml
+podman run --rm --name showroom-builder --platform linux/amd64 -v "./:/antora" docker.io/antora/antora zero-touch-site.yml
+#podman run --rm --name showroom-builder --platform linux/amd64 -v "./:/antora" docker.io/antora/antora site.yml
 echo "Build process complete. Check the ./www folder for the generated site."
+echo "To view the site locally, run the following command: utilities/serve.sh"
